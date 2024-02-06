6f1fa3f3943749ab2ea480d322f57d8772e8ff4f
Oleksandr Shcherbyna <sanya071186@gmail.com>
2024-02-06 17:44:47 +0200

    add year to footer

--- components/general/MfFooter.vue
+++ components/general/MfFooter.vue
@@ -1,6 +1,8 @@
 <template>
-  <el-footer class="bg-gray-500 p-4 text-white">
-    footer
+  <el-footer class="bg-gray-500 p-4 text-white flex justify-center items-center">
+    @{{ year }} Movie Finder
   </el-footer>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
