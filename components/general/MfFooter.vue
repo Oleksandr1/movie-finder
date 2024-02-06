6fc5788c8218a82c2c1af3e1c7e01e8859d2d66b
Oleksandr Shcherbyna <sanya071186@gmail.com>
2024-02-06 17:31:24 +0200

    add header and footer

---
+++ components/general/MfFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-footer class="bg-gray-500 p-4 text-white">
+    footer
+  </el-footer>
+</template>
+<script setup lang="ts"></script>
---
+++ components/general/MfHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="bg-gray-500 p-4 text-white">
+    <div class="container mx-auto flex flex-col sm:flex-row justify-between">
+      <h1 class="text-2xl font-semibold">MovieFinder</h1>
+      <div class="flex">
+        <mf-search />
+
+      </div>
+    </div>
+  </header>
+</template>
+<script setup lang="ts">
+import { ElLoading } from 'element-plus'
+import MfSearch from "~/components/general/MfSearch.vue";
+
+</script>
