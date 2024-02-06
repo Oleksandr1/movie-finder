43b5bca7196802ef1c783013936adf7cb4f13a0e
Oleksandr Shcherbyna <sanya071186@gmail.com>
2024-02-06 17:33:28 +0200

    use nuxt routing in main view

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div v-loading.fullscreen.lock="isLoad">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+<script setup lang="ts">
+const movieStore = useMovieStore();
+const { isLoad } = storeToRefs(movieStore);
+</script>
