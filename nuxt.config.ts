8b130d1be9830129d7fa7431b9f8a72bae9e5430
Oleksandr Shcherbyna <sanya071186@gmail.com>
2024-02-06 17:29:04 +0200

    add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@element-plus/nuxt', '@nuxtjs/eslint-module'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@element-plus/nuxt', '@nuxtjs/eslint-module', '@element-plus/nuxt'],
   // elementPlus: { /** Options */ }
 });
